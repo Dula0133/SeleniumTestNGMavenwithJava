98066125702f8dcfaaa748dde913b72246d5fd8b
Dula0133 <92773152+Dula0133@users.noreply.github.com>
2024-09-10 17:52:59 +0530

    test TestNG codes

---
+++ src/test/java/org/example/TestTestNG.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestTestNG {
+    @Test
+    public void exampleTest() {
+//        WebDriverManager.chromiumdriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        driver.quit();
+    }
+}
+
+
+
+
+
