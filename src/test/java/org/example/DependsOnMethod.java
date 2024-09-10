f086ff6aa751915cca1ae4359319c2bf58b79746
Dula0133 <92773152+Dula0133@users.noreply.github.com>
2024-09-10 21:55:09 +0530

    TestNG DependsonMethod test

---
+++ src/test/java/org/example/DependsOnMethod.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class DependsOnMethod {
+
+    @Test()
+    public void oLevel(){
+        System.out.println("oLevel");
+    }
+
+    @Test(priority = 1, dependsOnMethods = "oLevel")
+//    @Test(priority = 1, dependsOnMethods = "oLevel",enabled = false)campus test will not run give an error
+    public void aLevel(){
+        System.out.println("aLevel");
+    }
+
+    @Test(priority = 2,dependsOnMethods = "aLevel")
+    public void campus(){
+        System.out.println("campus");
+    }
+
+}
+
