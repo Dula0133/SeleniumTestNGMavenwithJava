46d5668cd094470c59c8b70208e586ad59b3e5b6
Dula0133 <92773152+Dula0133@users.noreply.github.com>
2024-09-10 17:35:15 +0530

    Add maven,selenium and testNG dependencies to pom file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        WebDriverManager.chromiumdriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        driver.quit();
+
+
     }
+
+
 }
+
